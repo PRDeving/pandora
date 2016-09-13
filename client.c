54cb8ffb3fcd10a08c2d69b499d51fd9bb5fa28d
prdeving <pablo.deving@gmail.com>
2016-09-13 18:07:25 +0200

    tests with key-value

--- client.c
+++ client.c
@@ -1,25 +1,21 @@
 #include <pandora.h>
 
 PND_HANDLER handle(PND_MESSAGE *msgs) {
-  pandora.emit("world!\0");
+  printf("Server said %s to connection", msgs -> msg);
 }
 
-int loop = 1;
-PND_HANDLER stopDigesting(PND_MESSAGE *msgs) {
-  printf("eysay stop\n");
-  loop = 0;
-  pandora.emit("stop\0");
+PND_HANDLER world(PND_MESSAGE *msg) {
+  printf("that fucker said %s\n", msg -> msg);
 }
-
 int main(int argc, char *argv[]) {
   printf("using %s\n\n", pandora.version);
   pandora.connect("localhost", 1337);
   pandora.info();
 
-  pandora.on("connection\0", handle);
-  pandora.on("stop\0", stopDigesting);
+  pandora.on("connection", handle);
+  pandora.on("hi", world);
 
-  while (loop) pandora.digest();
+  while (1) pandora.digest();
 
   pandora.close(PND_OK);
   return 0;
--- server.c
+++ server.c
@@ -1,9 +1,4 @@
 #include <pandora.h>
-
-/* PND_HANDLER world(PND_MESSAGE *msg) { */
-/*   printf("that fucker said woeld\n"); */
-/*   pandora.emit("stop\0"); */
-/* } */
 /*  */
 /* PND_HANDLER stop(PND_MESSAGE *msg) { */
 /*     pandora.close(0); */
@@ -12,18 +7,15 @@
 /* void* list(void* arg) { */
 /*   pandora.listen(1337); */
 /* } */
-
 int main() {
   printf("using %s\n\n", pandora.version);
 
-  /* pandora.on("world!\0", world); */
+  pandora.listen(1337);
 
-  /* pandora.listen(1337); */
-  /* pandora.info(); */
-
-  PND_deserialize_event("asdfjaowejflsdkfmþ↓ŧæasfiaf39fi3");
-
-  /* sleep(10); */
+  sleep(5);
+  pandora.emit("hi", "sucker pollens");
+  
+  sleep(10);
   pandora.close(0);
   exit(0);
 }
